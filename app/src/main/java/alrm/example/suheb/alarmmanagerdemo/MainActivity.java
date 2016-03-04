fc5e629fd87179100ac78498b86ad10beeabc1f6
suheb <suheb@redbytes.in>
2016-03-04 19:48:25 +0530

    first commit

---
+++ app/src/main/java/alrm/example/suheb/alarmmanagerdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package alrm.example.suheb.alarmmanagerdemo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by suheb on 4/3/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (MyBroadcastReceiver.mp!=null){
+                    MyBroadcastReceiver.mp.stop();
+                }
+                startAlert();
+            }
+        });
+    }
+    public void startAlert() {
+        EditText text = (EditText) findViewById(R.id.time);
+        int i = Integer.parseInt(text.getText().toString());
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (i * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm set in " + i + " seconds", Toast.LENGTH_LONG).show();
+    }
+}
